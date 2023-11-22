5fb793ed94f5f545ca47f968a59284a3e4d581f7
TarikBR <daniel.tarik.23@gmail.com>
2023-11-21 21:28:13 -0300

    fix: invalid property name

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,23 +3,23 @@ import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
     return (
-        <section class="page_404">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 ">
-                        <div class="col-sm-10 col-sm-offset-1  text-center">
-                            <div class="four_zero_four_bg">
-                                <h1 class="text-center ">404</h1>
+        <section className="page_404">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 ">
+                        <div className="col-sm-10 col-sm-offset-1  text-center">
+                            <div className="four_zero_four_bg">
+                                <h1 className="text-center ">404</h1>
                             </div>
 
-                            <div class="contant_box_404">
-                                <h3 class="h2">
+                            <div className="contant_box_404">
+                                <h3 className="h2">
                                     Parece que você está perdido
                                 </h3>
 
                                 <p>a página que você está tentando acessar não está disponível</p>
 
-                                <Link to='/' class="link_404">Página Inicial</Link>
+                                <Link to='/' className="link_404">Página Inicial</Link>
                             </div>
                         </div>
                     </div>
