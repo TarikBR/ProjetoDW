af5d42893c0e9770b7b977ca4e9bed15ccbdcfcd
TarikBR <daniel.tarik.23@gmail.com>
2023-11-27 00:16:47 -0300

    style: simple css in orders pannel
    Co-Authored-By: Eduardo Czekalski <100415316+Eduardoczekalaki@users.noreply.github.com>

--- src/components/OrdersPanel.js
+++ src/components/OrdersPanel.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../config/Config';
-import { collection, getDocs, doc, updateDoc, deleteDoc, getDoc } from 'firebase/firestore';
+import { collection, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 import { Navbar } from './Navbar';
 import { NotFound } from './NotFound';
 
@@ -26,36 +26,52 @@ export const OrdersPanel = ({ user, isAdmin }) => {
   const markAsCompleted = async (orderId) => {
     const orderRef = doc(db, 'Buyer-info', orderId);
     await updateDoc(orderRef, { Completed: true });
+
+    setOrders((prevOrders) => {
+      return prevOrders.map((order) =>
+        order.id === orderId ? { ...order, Completed: true } : order
+      );
+    });
   };
 
   const deleteOrder = async (orderId) => {
     const orderRef = doc(db, 'Buyer-info', orderId);
     await deleteDoc(orderRef);
+
+    setOrders((prevOrders) => prevOrders.filter((order) => order.id !== orderId));
   };
+
+  const completedOrders = orders.filter((order) => order.Completed);
+  const pendingOrders = orders.filter((order) => !order.Completed);
 
   return (
     <>
-      {!user && (<NotFound/>)}
+      {!user && <NotFound/>}
       {isAdmin && (
         <div>
-          <Navbar user={user}/>
-          <h2>Admin Panel</h2>
-          <table>
+          <Navbar user={user} isAdmin={true}/>
+          <h2>Pedidos</h2>
+
+          <div className="pending-banner">Pedidos Pendentes</div>
+          <table className="pending-order">
             <thead>
               <tr>
                 <th>Comprador</th>
                 <th>Email</th>
+                <th>Celular</th>
                 <th>Produtos</th>
+                <th>Total</th>
                 <th>Endereço</th>
                 <th>Concluída</th>
                 <th>Ações</th>
               </tr>
             </thead>
             <tbody>
-              {orders.map((order) => (
+              {pendingOrders.map((order) => (
                 <tr key={order.id}>
                   <td>{order.BuyerName}</td>
                   <td>{order.BuyerEmail}</td>
+                  <td>{order.BuyerCell}</td>
                   <td>
                     <ul>
                       {order.BuyerProducts.map((product) => (
@@ -65,10 +81,53 @@ export const OrdersPanel = ({ user, isAdmin }) => {
                       ))}
                     </ul>
                   </td>
+                  <td>R${order.BuyerPayment},00</td>
                   <td>{order.BuyerAddress}</td>
-                  <td>{order.Completed ? 'Sim' : 'Não'}</td>
+                  <td>Não</td>
                   <td>
                     <button onClick={() => markAsCompleted(order.id)}>Concluir</button>
+                    <button onClick={() => deleteOrder(order.id)}>Excluir</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+        <br/><br/><br/>
+
+          <div className="completed-banner">Pedidos Concluídos</div>
+          <table className="completed-order">
+            <thead>
+              <tr>
+                <th>Comprador</th>
+                <th>Email</th>
+                <th>Celular</th>
+                <th>Produtos</th>
+                <th>Total</th>
+                <th>Endereço</th>
+                <th>Concluída</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {completedOrders.map((order) => (
+                <tr key={order.id}>
+                  <td>{order.BuyerName}</td>
+                  <td>{order.BuyerEmail}</td>
+                  <td>{order.BuyerCell}</td>
+                  <td>
+                    <ul>
+                      {order.BuyerProducts.map((product) => (
+                        <li key={product.ProductName}>
+                          {product.ProductName} - {product.qty}
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                  <td>R${order.BuyerPayment},00</td>
+                  <td>{order.BuyerAddress}</td>
+                  <td>Sim</td>
+                  <td>
                     <button onClick={() => deleteOrder(order.id)}>Excluir</button>
                   </td>
                 </tr>
